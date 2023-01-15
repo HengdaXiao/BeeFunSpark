3bbccb36839620457a11650ddf21bc057afd08c3
Hengda Xiao <hengda.xiao@mehilainen.fi>
2023-01-15 21:56:15 +0200

    Update the UI

--- app/src/main/java/com/beehealthy/spothinta/SpotHintaWidget.kt
+++ app/src/main/java/com/beehealthy/spothinta/SpotHintaWidget.kt
@@ -3,10 +3,16 @@ package com.beehealthy.spothinta
 import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+import androidx.glance.GlanceModifier
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.background
 import androidx.glance.currentState
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
 import com.beehealthy.spothinta.network.HintaWorker
 import com.beehealthy.spothinta.network.PriceInfo
 import com.beehealthy.spothinta.network.PriceInfoStateDefinition
@@ -19,11 +25,14 @@ class SpotHintaWidget: GlanceAppWidget() {
     override fun Content() {
         val priceInfo = currentState<PriceInfo>()
         GlanceTheme() {
-                AppWidgetColumn() {
-                    Text(text = "Hello world!")
+                AppWidgetColumn(modifier = GlanceModifier.background(ImageProvider(R.drawable.ic_widget_background))) {
+                    Text(text = "Today", style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold, color = GlanceTheme.colors.onPrimary))
                     when(priceInfo){
                         is PriceInfo.Available -> {
-                            Text(text = "The first price is ${priceInfo.hourlyForecast.first().priceNoTax}")
+                            Text(text = "Lowest", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Medium, color = GlanceTheme.colors.onPrimary))
+                            Text(text = "${priceInfo.hourlyForecast.first().priceWithTax} snt/kWh", style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Medium, color = GlanceTheme.colors.primary))
+                            Text(text = "Highest", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Medium, color = GlanceTheme.colors.onPrimary))
+                            Text(text = "${priceInfo.hourlyForecast.last().priceWithTax} snt/kWh", style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Medium, color = GlanceTheme.colors.secondary))
                         }else ->{
                         Text(text = "Load price failed")
                     }
